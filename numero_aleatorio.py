3b57315c0619079965aff87a07e5e11ea167e2bd
João Pedro Viana <108822272+joaovianaa@users.noreply.github.com>
2022-11-29 00:08:41 -0300

    Update numero_aleatorio.py

--- numero_aleatorio.py
+++ numero_aleatorio.py
@@ -9,7 +9,7 @@ numero_tentativas = 3
 for tentativa in range(1, numero_tentativas +1):
     numero_escolhido = int(input('Escolha um número: '))
     if numero_escolhido == numero_adivinhado:
-        print(f'Parabéns! Você acertou em {tentativa}, tentativas!')
+        print(f'Parabéns! Você acertou em {tentativa} tentativas!')
         break
     elif numero_escolhido > numero_adivinhado:
         print('Escolha um número menor!')
